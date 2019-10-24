abf2a80ad5a3345849e9b8de63923f60df635f32
Daniela Maksoud <daniela.brazao.maksoud@gmail.com>
2019-10-24 18:33:23 -0300

    Create Prescription-v1.sol

---
+++ Prescription-v1.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.8; // Solidity Version
+
+contract Prescription { // Contract
+
+	// Store medication
+	// Read medication
+
+	// Variables
+	string public medication;
+
+	// Constructor
+	constructor() public {
+		medication = "Ibuprofeno (Comprimido 300 mg)";		
+	}
+
+}
